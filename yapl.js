d89ed91029858aa11d589e3ec93fd0c2d31b9320
Matt Diehl <mdiehl1@umbc.edu>
2014-12-15 17:56:15 -0500

    further work on rewrite; wip

--- yapl.js
+++ yapl.js
@@ -4,6 +4,7 @@
 var fs = require('fs'),
     path = require('path'),
     glob = require('glob'),
+    assemble = require('assemble'),
     handlebars = require('handlebars'),
     helpers = require('handlebars-helpers'),
     extend = require('node.extend'),
@@ -13,7 +14,7 @@ var fs = require('fs'),
     _ = require('lodash');
 
 // YAPL Internal Variables
-var configObj = {
+var config = {
     settings: {
         cssBlockRegEx: /\/\*\s*?YAPL\n([\s\S]*?)\*\//g,
         htmlBlockRegEx: /\<!--\s*?YAPL\n([\s\S]*?)--\>/g,
@@ -40,10 +41,15 @@ function YAPL(options) {
     // Build Prep Steps
     extendConfig(options);
     setupHandlebarsConfig();
+    setupAssembleConfig();
     createAllSectionObjects();
-    createAllSectionChildrenObjects();
-    createAllDisplayTemplateObjects();
-    createAllImageSizeObjects();
+    // -> createSingleSectionObj
+        // -> createAllSectionChildrenObjects
+            // -> createSingleSectionChildObject
+                // -> parseYAPLJsonFromFile
+                    // -> createSingleYAPLBlockObject
+    createAllDisplayTemplateObjects(); // incomplete
+    createAllImageSizeObjects(); // incomplete
 
     // Section-level Build
     buildAllHtmlExamples();
@@ -51,8 +57,12 @@ function YAPL(options) {
     // Cross Linking
     crossLinkSectionChildren();
 
+    // Output JSON to file if set as option
+    outputConfigToFile();
+
     // Pattern Library Build
     buildPatternLibrary();
+
 }
 
 
@@ -62,13 +72,14 @@ function extendConfig(options) {
     console.log('extendConfig');
     // ============================
 
-    configObj = extend(true, configObj, options);
+    config = extend(true, config, options);
 }
 
 function setupHandlebarsConfig() {
     console.log('setupHandlebarsConfig');
     // ============================
-    var partials = glob.sync(configObj.settings.partials);
+
+    var partials = glob.sync(config.settings.partials);
     // register built-in helpers
     if (helpers && helpers.register) {
         helpers.register(handlebars, {}, {});
@@ -81,12 +92,18 @@ function setupHandlebarsConfig() {
     });
 }
 
+function setupAssembleConfig() {
+    console.log('setupAssembleConfig');
+    // ============================
+
+}
+
 function createAllSectionObjects() {
     console.log('createAllSectionObjects');
     // ============================
 
-    configObj.sections.forEach(function(section, index) {
-        configObj.sections[index] = createSingleSectionObj(section);
+    config.sections.forEach(function(section, index) {
+        config.sections[index] = createSingleSectionObj(section);
     });
 }
 
@@ -94,33 +111,55 @@ function createSingleSectionObj(obj) {
     console.log('createSingleSectionObj');
     // ============================
 
-    var sectionObj = {};
+    var sectionObject = {};
 
-    sectionObj['name'] = obj.name || 'Undefined Section';
-    sectionObj['nameCamelCase'] = utils.camelCase(sectionObj['name']);
-    sectionObj['nameCssCase'] = utils.cssCase(sectionObj['name']);
-    sectionObj['landingTemplate'] = obj.landingTemplate || false;
-    sectionObj['childTemplate'] = obj.childTemplate || false;
-    sectionObj['css'] = obj.css || false;
-    sectionObj['partials'] = obj.partials || configObj.settings.partials;
-    sectionObj['data'] = obj.data || configObj.settings.data;
-    sectionObj['link'] = sectionObj.landingTemplate ? path.join(configObj.settings.buildDir, sectionObj.nameCssCase, 'index.html') : false;
-    sectionObj['cssFiles'] = sectionObj['css'] ? glob.sync(sectionObj['css']) : false;
-    sectionObj['children'] = [];
+    sectionObject['name'] = obj.name || 'Undefined Section';
+    sectionObject['nameCamelCase'] = utils.camelCase(sectionObject['name']);
+    sectionObject['nameCssCase'] = utils.cssCase(sectionObject['name']);
+    sectionObject['landingTemplate'] = obj.landingTemplate || false;
+    sectionObject['childTemplate'] = obj.childTemplate || false;
+    sectionObject['css'] = obj.css || false;
+    sectionObject['partials'] = obj.partials || config.settings.partials;
+    sectionObject['data'] = obj.data || config.settings.data;
+    sectionObject['link'] = sectionObject.landingTemplate ? path.join(config.settings.buildDir, sectionObject.nameCssCase, 'index.html') : false;
+    sectionObject['cssFiles'] = sectionObject['css'] ? glob.sync(sectionObject['css']) : false;
+    sectionObject['children'] = sectionObject['cssFiles'] ? createAllSectionChildrenObjects(sectionObject['cssFiles'], sectionObject.nameCssCase) : false;
 
-    return sectionObj;
+    return sectionObject;
 }
 
-function createAllSectionChildrenObjects() {
+function createAllSectionChildrenObjects(cssFiles, sectionName) {
     console.log('createAllSectionChildrenObjects');
     // ============================
 
+    var childrenObjects = [];
+
+    cssFiles.forEach(function(cssFile) {
+        var childObject = createSingleSectionChildObject(cssFile, sectionName);
+        childObject && childrenObjects.push(childObject);
+    });
+
+    return childrenObjects;
 }
 
-function createSingleSectionChildObject(obj) {
+function createSingleSectionChildObject(cssFile, sectionName) {
     console.log('createSingleSectionChildObject');
     // ============================
 
+    var childObject = {},
+        cssFileExt = path.extname(cssFile),
+        cssFileBasename = path.basename(cssFile, cssFileExt).replace('_', ''),
+        childObjectFilename = cssFileBasename + '.html';
+
+    childObject['name'] = utils.titleCase(cssFileBasename);
+    childObject['nameCamelCase'] = utils.camelCase(cssFileBasename);
+    childObject['nameCssCase'] = cssFileBasename;
+    childObject['link'] = path.join(config.settings.buildDir, sectionName, childObjectFilename);
+    childObject['blocks'] = parseYAPLJsonFromFile(cssFile, childObject['link']);
+
+    if (childObject['blocks'] && childObject['blocks'].length) {
+        return childObject;
+    }
 }
 
 function createAllDisplayTemplateObjects() {
@@ -147,17 +186,75 @@ function createSingleImageSizeObjects() {
 
 }
 
+function parseYAPLJsonFromFile(file, link) {
+    console.log('parseYAPLBlocksFromContent');
+    // ============================
+
+    var fileExt = path.extname(file),
+        fileContent = fs.readFileSync(file, 'utf8'),
+        regEx = _.contains(fileExt, 'html') ? config.settings.htmlBlockRegEx :
+                _.contains(fileExt, 'css') ? config.settings.cssBlockRegEx : false,
+        YAPLBlocks = fileContent.match(regEx),
+        YAPLJson = [];
+
+    if (YAPLBlocks && YAPLBlocks.length) {
+        YAPLBlocks.forEach(function(YAPLBlock) {
+            var yamlString,
+                json;
+
+            yamlString = YAPLBlock.replace(regEx, function(match, p1) {
+                return p1;
+            });
+            json = createSingleYAPLBlockObject(yaml.safeLoad(yamlString), link);
+            YAPLJson.push(json);
+        });
+        return YAPLJson;
+    } else {
+        return false;
+    }
+}
+
+function createSingleYAPLBlockObject(obj, link) {
+    var blockObj = {};
+
+    blockObj['name'] = obj.name || 'Undefined Name';
+    blockObj['nameCamelCase'] = utils.camelCase(blockObj['name']);
+    blockObj['nameCssCase'] = utils.cssCase(blockObj['name']);
+    blockObj['notes'] = obj.notes || false;
+    blockObj['partial'] = obj.partial || blockObj['nameCamelCase'];
+    blockObj['context'] = obj.context || false;
+    blockObj['selector'] = obj.selector || false;
+    blockObj['link'] = link + '#' + blockObj['nameCssCase'];
+    blockObj['html'] = '<html>';
+    blockObj['references'] = {};
+
+    return blockObj;
+}
+
 
 
 // Section-Level Build
 
 function buildAllHtmlExamples() {
     console.log('buildAllHtmlExamples');
-    // ============================\
+    // ============================
 
+    config.sections.forEach(function(section) {
+        if (section.children && section.children.length) {
+            section.children.forEach(function(sectionChild) {
+                if (sectionChild.blocks && sectionChild.blocks.length) {
+                    sectionChild.blocks.forEach(function(block) {
+
+                        block['html'] = buildSingleHtmlExample(block, sectionChild);
+
+                    });
+                }
+            });
+        }
+    });
 }
 
-function buildSingleHtmlExample() {
+function buildSingleHtmlExample(block, sectionChild) {
     console.log('buildSingleHtmlExample');
     // ============================
 
@@ -171,6 +268,19 @@ function crossLinkSectionChildren() {
     console.log('crossLinkSectionChildren');
     // ============================
 
+}
+
+
+
+function outputConfigToFile() {
+    console.log('outputConfigToFile');
+    // ============================
+
+    var outputPath = config.settings.outputJSONFile,
+        outputDir = path.dirname(outputPath),
+        outputFilename = path.basename(outputPath)
+
+    //fs.exists();
 }
 
 
@@ -206,7 +316,9 @@ YAPL({
         displayTemplates: 'example/ProductionTemplates/**/*.html',
         buildDir: 'example/styleguide',
         index: 'lib/templates/index.hbs',
-        outputJSONFile: 'example/styleguide.json'
+        outputJSONFile: 'example/styleguide.json',
+        libraryLayout: 'example/templates-styleguide/layouts/default.hbs',
+        libraryPartials: 'example/templates-styleguide/partials/**/*.hbs'
     },
     sections: [{
         name: 'Micro Elements',
@@ -217,12 +329,11 @@ YAPL({
         name: 'Macro Elements',
         landingTemplate: 'lib/templates/macro-element-landing.hbs',
         childTemplate: 'lib/templates/macro-element.hbs',
-        css: 'example/css/modules/macro/**/*.scss',
+        css: 'example/css/modules/macro/**/*.scss'
     }, {
         name: 'Layouts',
         landingTemplate: 'lib/templates/layouts-landing.hbs',
-        childTemplate: 'lib/templates/layout.hbs',
-        css: 'example/css/modules/macro/**/*.scss',
+        childTemplate: 'lib/templates/layout.hbs'
     }, {
         name: 'Display Templates',
         landingTemplate: 'lib/templates/display-templates-landing.hbs'
